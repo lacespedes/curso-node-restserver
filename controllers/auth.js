09dea6d54cd55dc1d4d85a0ee791c71c96d0327d
Leonardo Cespedes <leonardo.cespedes@gmail.com>
2023-02-12 13:11:08 -0500

    Fin sección 11 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const {response} = require('express');
+const {response, json} = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -45,6 +46,53 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSingIn = async(req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const {email, name, img} = await googleVerify(id_token);
+
+        let user = await User.findOne({email});
+        if (!user){
+            // Tengo que crearlo
+            const data = {
+                name,
+                email,
+                password: ' ',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            };
+
+            user = new User(data);
+            await user.save();
+            
+        }
+
+        if (!user.status) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador - usuariob loqueado'
+            });            
+        }
+
+        const token = await generarJWT( user.id );
+
+        res.json({
+            user,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
